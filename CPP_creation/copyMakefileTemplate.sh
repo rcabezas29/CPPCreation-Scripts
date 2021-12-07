a33cc87dcf7d4a728d4d957787090bd085b1b60c
Raúl Cabezas González <58788516+rcabezas29@users.noreply.github.com>
2021-12-07 12:48:57 +0100

    Update copyMakefileTemplate.sh

--- CPP_creation/copyMakefileTemplate.sh
+++ CPP_creation/copyMakefileTemplate.sh
@@ -3,7 +3,7 @@ CPP_NAME=$(echo $1.cpp)
 DIR=$(dirname $0)
 
 cp $DIR/templates/Makefile .
-sed -i '' "s/NAME = /NAME = $PROGRAM_NAME/" $DIR/templates/Makefile
-sed -i '' "s/main.cpp/main.cpp $CPP_NAME/" $DIR/templates/Makefile
+sed -i '' "s/NAME = /NAME = $PROGRAM_NAME/" Makefile
+sed -i '' "s/main.cpp/main.cpp $CPP_NAME/" Makefile
 vim -c ":Stdheader" Makefile -c ":wq"
 
